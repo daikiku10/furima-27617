072ce92085900e1ab946480710460593facd29d0
daikiku10 <soccerdk19.910@gmail.com>
2020-10-27 17:03:41 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,24 @@ class Item < ApplicationRecord
   belongs_to_active_hash :delivery_day
 
   belongs_to :user
+
+  with_options presence: true do
+    validates :name
+    validates :explain
+    validates :price, numericality: { only_integer: true, greater_than: 300, less_than: 9999999, message: "Out of setting range"}, format: {with: /^[0-9]+$/, message: "Half-width number"}
+    validates :category
+    validates :status
+    validates :delivery_burden
+    validates :delivery_area
+    validates :delivery_day
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :delivery_burden_id
+    validates :delivery_area_id
+    validates :delivery_day_id
+  end
+
 end
