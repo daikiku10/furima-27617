ac4ae4980f64df785d5781cb11d31d353142f83d
daikiku10 <soccerdk19.910@gmail.com>
2020-10-27 11:35:55 +0900

    バリデーション with_options使用

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :password, format: {with: /\A(?=.*?[a-z])[a-z\d]+\z/, message: "英数字を必ず含めてください"}
-  validates :nickname, presence: true
-  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ンー-龥]/, message: "全角で入力してください！"}
-  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ンー-龥]/, message: "全角で入力してください！"}
-  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー-]+\z/, message: "全角（カタカナ）で入力してください！"}
-  validates :first_name_kana, presence:true, format: {with: /\A[ァ-ヶー-]+\z/, message: "全角（カタカナ）で入力してください！"}
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :last_name, format: {with: /\A[ぁ-んァ-ンー-龥]/, message: "全角で入力してください！"}
+    validates :first_name, format: {with: /\A[ぁ-んァ-ンー-龥]/, message: "全角で入力してください！"}
+    validates :last_name_kana, format: {with: /\A[ァ-ヶー-]+\z/, message: "全角（カタカナ）で入力してください！"}
+    validates :first_name_kana, format: {with: /\A[ァ-ヶー-]+\z/, message: "全角（カタカナ）で入力してください！"}
+    validates :birthday
+  end
 end
