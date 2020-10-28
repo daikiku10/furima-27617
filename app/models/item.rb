665f161482a92f6ec128ca5c784da788cc8e8144
daikiku10 <soccerdk19.910@gmail.com>
2020-10-28 17:21:24 +0900

    修正①　バリデーション変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,10 +16,10 @@ class Item < ApplicationRecord
     validates :explain
     validates :price, numericality: { only_integer: true, greater_than: 300, less_than: 9999999, message: "Out of setting range"}, format: {with: /\A[0-9]+\z/, message: "Half-width number"}
     validates :category
-    validates :status
-    validates :delivery_burden
-    validates :delivery_area
-    validates :delivery_day
+    validates :status_id
+    validates :delivery_burden_id
+    validates :delivery_area_id
+    validates :delivery_day_id
   end
 
   with_options numericality: { other_than: 1, message: 'Select'} do
