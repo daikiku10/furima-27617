afc0cc53a46318120a9fd4915dcaf23e1e6219e9
daikiku10 <soccerdk19.910@gmail.com>
2020-10-30 14:49:43 +0900

    コードの省略化

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :cheak_user, only: :edit
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -42,9 +40,13 @@ class ItemsController < ApplicationController
   end
 
   def cheak_user
-    @item = Item.find(params[:id])
-    unless current_user.id == @item.user_id
+    item = Item.find(params[:id])
+    unless current_user.id == item.user_id
       redirect_to root_path
     end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
