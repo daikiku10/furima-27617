287b222e4c6545a54cff01848c0fc2ad02e1eaf0
daikiku10 <soccerdk19.910@gmail.com>
2020-11-02 22:16:21 +0900

    エラーメッセージの表示

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -4,10 +4,10 @@ class OrderForm
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: { other_than: 1, message: "Select" }
+    validates :prefecture_id, numericality: {other_than: 0, message: "Select" }
     validates :city
     validates :house_number
-    validates :phone_number
+    validates :phone_number, format: {with: /\A[0-9]\z/, message: 'input only number'}, length: { maximum: 11, } 
   end
 
   def save
