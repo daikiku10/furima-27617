2ec3925717d10951f6c699872387f87afd68611d
daikiku10 <soccerdk19.910@gmail.com>
2020-11-02 18:59:12 +0900

    データベースへ保存（orders,addresses）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,23 @@
 class OrdersController < ApplicationController
 
   def index
+    @order_form = OrderForm.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @order_form = OrderForm.new(form_params)
+    if @order_form.valid?
+      @order_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def form_params
+    params.require(:order_form).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
   
 end
